49126e63c233850d0062b93608f55ef976dcc3c3
Miguel Torres <migueltorreslopez@gmail.com>
2016-11-12 17:26:40 -0800

    Add rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+extend crate rand;
+
 use std::io;
+use rand::Rng;
+
 
 fn main() {
     println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().get_range(1, 101)
+    
+    println!("The secret number is {}", secret_number);
+    
     println!("Please input your guess");
     
     let mut guess = String::new();
